c3d3342f001f337d5466da1e6aa2a58598a678d1
Md Mahfuzur Rahman <shawon922@gmail.com>
2018-01-22 16:40:06 +0600

    Added stable bootstrap 4 and implemented dynamic menu

--- categories/models.py
+++ categories/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     parent = models.ForeignKey(
-        'self', on_delete=models.PROTECT, blank=True, null=True)
+        'self', on_delete=models.PROTECT, blank=True, null=True, related_name='categories')
     updated_at = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from categories.models import Category
+from products.models import Product
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    categories = Category.objects.filter(parent=None).all()
+
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'home.html', context)
 
 
 def backend_home(request):
